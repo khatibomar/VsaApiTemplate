811e8727deb360b724850e6c6f680c80c5c8a663
Omar ElKhatib <elkhatibomar@outlook.com>
2024-01-01 22:35:45 +0200

    add services reference to API project

--- Web/Api/Controllers/v1/FeaturesController.cs
+++ Web/Api/Controllers/v1/FeaturesController.cs
@@ -1,5 +1,22 @@
-﻿namespace Api.Controllers.v1;
+﻿using Microsoft.AspNetCore.Mvc;
+using VsaTemplate.Features.Models;
+using VsaTemplate.Features.Services;
 
-public class FeaturesController
+namespace Api.Controllers.v1;
+
+[ApiController]
+[Route("/features")]
+public class FeaturesController : ControllerBase
 {
+	private FeaturesService _featuresService { get; set; }
+
+	public FeaturesController(FeaturesService featuresService)
+	{
+		_featuresService = featuresService;
+	}
+
+	public ActionResult<IEnumerable<Feature>> Get()
+	{
+		return Ok(_featuresService.GetFeatures());
+	}
 }
